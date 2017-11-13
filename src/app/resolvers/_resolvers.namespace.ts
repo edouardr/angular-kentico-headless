0351543465ec2833a2b64fea6d1a311ffcc8e680
edouardr <riviere.edouard@hotmail.fr>
2017-11-13 23:29:17 +1100

    Remove old namespace files

--- src/app/resolvers/_resolvers.namespace.ts
+++
@@ -1,5 +0,0 @@
-import { ArticleToViewModelResolver } from './article-to-view-model-resolver.func';
-import { HeroUnitToViewModelResolver } from './hero-unit-to-view-model-resolver.func';
-import { CafeToViewModelResolver } from './cafe-to-view-model-resolver.func';
-
-export { ArticleToViewModelResolver, HeroUnitToViewModelResolver, CafeToViewModelResolver };
--- src/app/view-models/_view-models.namespace.ts
+++
@@ -1,5 +0,0 @@
-import { ArticleVM } from './article-vm.class';
-import { HeroUnitVM } from './hero-unit-vm.class';
-import { CafeVM } from './cafe-vm.class';
-
-export { ArticleVM, HeroUnitVM, CafeVM };
